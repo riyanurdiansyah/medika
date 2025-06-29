53749f8fb5ca5f61a9cecd69b9ba9796d30dbecb
riyan.nurdiansyah <riyan.nurdiansyah@kalbenutritionals.com>
2025-06-29 07:40:58 +0700

    up lg

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const path = require('path')
 // Remove this if you're not using Fullcalendar features
 
 module.exports = {
+  trailingSlash: true,
   reactStrictMode: false,
   webpack: config => {
     config.resolve.alias = {
